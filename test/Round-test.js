c596c548b66da94844ca77522ef9ce16114f643b
kendallha <kendallhaw@gmail.com>
2021-03-23 10:59:58 -0700

    Add tests for Round class methods

--- test/Round-test.js
+++ test/Round-test.js
@@ -13,14 +13,14 @@ describe('Round', function() {
     let card3;
     let deck;
 
-    beforeEach(function() {
-        card1 = new Card(1, 'Which of the following is not an array prototype method?', ['array.filter', 'array.map', 'array.extrapolate'], 'array.extrapolate');
-        card2 = new Card(2, 'What is the index value of the first element in an array?', ['1', '0', 'null'], '0');
-        card3 = new Card(3, 'Which of these is a complex data type in JS?', ['boolean', 'object', 'string'], 'object');
+    // beforeEach(function() {
+    //     card1 = new Card(1, 'Which of the following is not an array prototype method?', ['array.filter', 'array.map', 'array.extrapolate'], 'array.extrapolate');
+    //     card2 = new Card(2, 'What is the index value of the first element in an array?', ['1', '0', 'null'], '0');
+    //     card3 = new Card(3, 'Which of these is a complex data type in JS?', ['boolean', 'object', 'string'], 'object');
 
-        deck = new Deck([card1, card2, card3]);
-        round = new Round(deck);
-    });
+    //     deck = new Deck([card1, card2, card3]);
+    //     round = new Round(deck);
+    // });
 
     it('should be a function', function() {
         expect(Round).to.be.a('function');
@@ -31,16 +31,40 @@ describe('Round', function() {
     });
 
     it('should have a current card that starts as the first card in the deck', function() {
-        expect(round.currentCard).to.equal(deck[0]);
+        expect(round.currentCard).to.deep.equal(deck[0]);
     });
 
     it('should be able to return the current card', function() {
+        round.returnCurrentCard();
         expect(round.returnCurrentCard()).to.equal(round.currentCard);
     });
 
     it('should create a new turn instance when a turn is taken', function() {
-        expect(round.takeTurn()).to.equal.
-    })
+        round.takeTurn('array.filer');
+        expect(turn).to.be.an.instanceOf(Turn);
+    });
+
+    it('should update the turn counter if the guess is incorrect', function() {
+        round.takeTurn('array.map');
+        expect(round.turns).to.equal(1);
+    });
+
+    it('should update the turn counter if the guess is correct', function() {
+        round.takeTurn('array.extrapolate');
+        expect(round.turns).to.equal(1);
+    });
+
+    it('should update the current card to the next in the deck after a turn is taken', function() {
+        round.takeTurn('array.map');
+        expect(round.currentCard).to.deep.equal(deck[1]);
+
+        round.takeTurn('1');
+        expect(round.currentCard).to.deep.equal(deck[2]);
+    });
+
+    it('should recor')
+
+
 
 
 })
